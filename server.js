e521378302a12e37d566abe61294477fb2570d5d
Lorenzo <lorenzoahenrique@icloud.com>
2025-02-12 12:07:43 -0300

    Changed visual and buttons added

--- server.js
+++ server.js
@@ -33,3 +33,10 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
